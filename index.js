46a8a91765d5092d2c9446e37f636fa532ba81f6
shahjalal <shahjalalkhan215@gmail.com>
2021-09-21 15:31:29 +0600

    filter project

--- index.js
+++ index.js
@@ -1,56 +1,56 @@
- 
-const tagsEl = document.getElementById('tags')
-const textArea = document.getElementById('textarea')
+const result = document.getElementById('result')
+const filter = document.getElementById('filter')
+const listItems = []
 
- textArea.addEventListener('keyup', (e) => {
-    createTextArea(e.target.value)
-    if(e.key === 'Enter'){
-      setTimeout(() => {
-        e.target.value = ''
-      }, 10)
+getData()
+filter.addEventListener('input', (e) => {
+    filterData(e.target.value)
+})
+async function getData(){
+    const res = await fetch('https://randomuser.me/api?results=1000')
+    const {results} = await res.json()
 
-      randomSelect()
-    }
- })
+    results.forEach(user => {
+        const li = document.createElement('li')
+        listItems.push(li)
+        li.innerHTML = `
+        <img src="${user.picture.large}" alt="${user.name.first}">
+        <div class="user-info">
+            <h4>${user.name.first} ${user.name.last}</h4>
+            <p>${user.location.city}, ${user.location.country}</p>
+        </div>
+        `
+        result.appendChild(li)
+    })
+}
 
- const createTextArea = (input) => {
-     tagsEl.innerHTML = ''
-   const tags = input.split(',').filter(tag => tag.trim() !== '').map(tag => tag.trim())
-   tags.forEach(item => {
-       const tagEl = document.createElement('span')
-       tagEl.className = 'tag'
-       tagEl.innerText = item
-       tagsEl.appendChild(tagEl)
-   })   
- }
+const filterData = (searchTerm) => {
+    listItems.forEach(item => {
+        if(item.innerText.toLowerCase().includes(searchTerm.toLowerCase())){
+            item.classList.remove('hide')
+        }else{
+            item.classList.add('hide')
+        }
+    })
+}
 
- const randomSelect = () => {
-   const times = 30
-   const interval = setInterval(() => {
-     const randamTag = pickRandomTag()
-     highLightTag(randamTag)
-     setTimeout(() => {
-       unHighLightTag(randamTag)
-     }, 100)
-   }, 100)
-   setTimeout(() => {
-     clearInterval(interval)
-    setTimeout(() => {
-      const randamTag = pickRandomTag()
-      highLightTag(randamTag)
-    }, 100)
 
-   }, times * 100)
- }
 
- const pickRandomTag = () => {
-   const tags = document.querySelectorAll('.tag')
-   return tags[Math.floor(Math.random() * tags.length)]   
- }
 
- const highLightTag = (tag) => {
-   tag.classList.add('heighlight')
- }
- const unHighLightTag = (tag) => {
-   tag.classList.remove('heighlight')
- }
+
+
+
+
+
+
+
+
+
+
+//  <li>
+//         <img src="https://randomuser.me/api/portraits/women/33.jpg" alt="Sara">
+//         <div class="user-info">
+//           <h4>Sara Smith</h4>
+//           <p>Wexford, Ireland</p>
+//         </div>
+//       </li>
