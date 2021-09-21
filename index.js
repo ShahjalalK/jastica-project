0278caf55076a81f79efb2e457a10cd7d25eface
shahjalal <shahjalalkhan215@gmail.com>
2021-09-21 19:07:24 +0600

    Contant Placeholder

--- index.js
+++ index.js
@@ -1,56 +1,30 @@
-const result = document.getElementById('result')
-const filter = document.getElementById('filter')
-const listItems = []
+const cardHeader = document.querySelector('#header')
+const title = document.querySelector('#title')
+const excerpt = document.querySelector('#excerpt')
+const profileImg = document.querySelector('#profile_img')
+const name = document.querySelector('#name')
+const date = document.querySelector('#date')
 
-getData()
-filter.addEventListener('input', (e) => {
-    filterData(e.target.value)
-})
-async function getData(){
-    const res = await fetch('https://randomuser.me/api?results=1000')
-    const {results} = await res.json()
+const animatedBg = document.querySelectorAll('.animated-bg')
+const animatedBgText = document.querySelectorAll('.animated-bg-text')
 
-    results.forEach(user => {
-        const li = document.createElement('li')
-        listItems.push(li)
-        li.innerHTML = `
-        <img src="${user.picture.large}" alt="${user.name.first}">
-        <div class="user-info">
-            <h4>${user.name.first} ${user.name.last}</h4>
-            <p>${user.location.city}, ${user.location.country}</p>
-        </div>
-        `
-        result.appendChild(li)
-    })
+const getData = () => {
+    cardHeader.innerHTML = `
+    <img
+    src="https://images.unsplash.com/photo-1593642632823-8f785ba67e45?ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80"
+    alt="">
+    `
+    title.innerHTML = `Lorem ipsum dolor sit amet.`
+    excerpt.innerHTML = `Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos, officiis?`
+    profileImg.innerHTML = `<img src="https://randomuser.me/api/portraits/men/45.jpg" alt="">`
+    name.innerHTML = `Jhone Doe`
+    date.innerHTML = 'Oct 08, 2020'
+    animatedBg.forEach(bg => bg.classList.remove('animated-bg'))
+    animatedBgText.forEach(bgText => bgText.classList.remove('animated-bg-text'))
 }
 
-const filterData = (searchTerm) => {
-    listItems.forEach(item => {
-        if(item.innerText.toLowerCase().includes(searchTerm.toLowerCase())){
-            item.classList.remove('hide')
-        }else{
-            item.classList.add('hide')
-        }
-    })
-}
+setTimeout(() => {
+    getData()
+}, 5000)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-//  <li>
-//         <img src="https://randomuser.me/api/portraits/women/33.jpg" alt="Sara">
-//         <div class="user-info">
-//           <h4>Sara Smith</h4>
-//           <p>Wexford, Ireland</p>
-//         </div>
-//       </li>
+console.log(date)
